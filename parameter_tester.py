2532ec4749f3a2042f8aca9b0398441aad4aba54
AKA-YL <104190275+AKA-YL@users.noreply.github.com>
2023-07-20 00:53:36 +0530

    Create parameter_tester.py
    Uses Pipeline and GridSearchCV to explore and find best parameters for text feature vectorizer and classifier

---
+++ parameter_tester.py
@@ -0,0 +1,52 @@
+absolute_path = os.path.dirname(__file__)
+relative_path = "test_data.csv"
+full_path = os.path.join(absolute_path, relative_path)
+df = pd.read_csv(full_path)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    df['descriptor'], df['item'], shuffle=True, test_size=0.2, random_state=None)
+
+# all the code following this is to test various parameters for the vectorizer and classifier
+# and identify ideal parameters
+pipeline = Pipeline([("vect", TfidfVectorizer()), ("clf", MultinomialNB())])
+
+parameter_grid = {
+    "vect__max_df": (0.2, 0.4, 0.6, 0.8, 1.0),
+    "vect__min_df": (1, 3, 5, 10),
+    "vect__ngram_range": ((1, 1), (1, 2)),  # unigrams or bigrams
+    "vect__norm": ("l1", "l2"),
+    "clf__alpha": np.logspace(-6, 6, 13),
+}
+
+from pprint import pprint
+
+from sklearn.model_selection import GridSearchCV
+
+grid_search = GridSearchCV(
+    estimator=pipeline,
+    param_grid=parameter_grid,
+    n_jobs=2,
+    verbose=1,
+)
+
+print("Performing grid search...")
+print("Hyperparameters to be evaluated:")
+pprint(parameter_grid)
+
+from time import time
+
+t0 = time()
+grid_search.fit(X_train, y_train)
+print(f"Done in {time() - t0:.3f}s")
+
+print("Best parameters combination found:")
+best_parameters = grid_search.best_estimator_.get_params()
+for param_name in sorted(parameter_grid.keys()):
+    print(f"{param_name}: {best_parameters[param_name]}")
+    
+test_accuracy = grid_search.score(X_test, y_test)
+print(
+    "Accuracy of the best parameters using the inner CV of "
+    f"the random search: {grid_search.best_score_:.3f}"
+)
+print(f"Accuracy on test set: {test_accuracy:.3f}")
