34e4edf2bc52f0dd1efa0dd6e3c73fed16404deb
Zixon7 <chikaosro@gmail.com>
2023-08-30 22:23:50 -0400

    Added node project for testing model

---
+++ node_project/index.js
@@ -0,0 +1,26 @@
+// try doing "npm install @tensorflow/tfjs-node" in the terminal
+//then run "node index.js" in the terminal
+
+const tf = require("@tensorflow/tfjs-core");
+const modelPath = './model.json';
+
+//should be:
+//const tf = require('@tensorflow/tfjs-node');
+//const modelPath = './path/to/tfjs/model/model.json';
+
+
+async function loadModel() {
+  const loadedModel = await tf.loadLayersModel(`file://${modelPath}`);
+  return loadedModel;
+}
+
+async function predict() {
+  const loadedModel = await loadModel();
+  const inputData = tf.tensor2d([[...your_input_data]], [1, ...input_shape]);
+  const outputTensor = loadedModel.predict(inputData);
+  const outputData = Array.from(await outputTensor.data());
+  console.log('Predictions:', outputData);
+}
+
+predict();
+console.log('hi');
